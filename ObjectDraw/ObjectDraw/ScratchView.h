155b4942b537c14e5417c225e47dce3276ab0bdb
chriswain <chriswainwrightaaron@gmail.com>
2015-06-01 16:13:46 -0400

    what is this

---
+++ ObjectDraw/ObjectDraw/ScratchView.h
@@ -0,0 +1,13 @@
+//
+//  ScratchView.h
+//  ObjectDraw
+//
+//  Created by Christopher Wainwright Aaron on 6/1/15.
+//  Copyright (c) 2015 Christopher Wainwright Aaron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScratchView : UIView
+
+@end
---
+++ ObjectDraw/ObjectDraw/ScratchView.m
@@ -0,0 +1,21 @@
+//
+//  ScratchView.m
+//  ObjectDraw
+//
+//  Created by Christopher Wainwright Aaron on 6/1/15.
+//  Copyright (c) 2015 Christopher Wainwright Aaron. All rights reserved.
+//
+
+#import "ScratchView.h"
+
+@implementation ScratchView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
